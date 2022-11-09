8d5fc27ffb85aa80fe1793103bf735b6763b18ce
Jack Davison <davison.jack.jd@gmail.com>
2022-11-09 08:04:53 +0000

    feat (theme_polar.R): panel_ontop
    Allow users to choose whether to put the panel on top (good for polar plots, less good for roses)

--- R/theme_polar.R
+++ R/theme_polar.R
@@ -7,15 +7,18 @@
 #' @param guides Resize the legend? Defaults to `TRUE`. `FALSE` may be useful if
 #'   you are controlling the legend yourself with [ggplot2::theme()] and/or
 #'   [ggplot2::guides()], but still want to apply the axis formatting.
+#' @param panel_ontop Place panel grids on top of the plot geometries? The
+#'   default, `TRUE`, can make polar plots easier to interpret, but this
+#'   behaviour may not always be desired.
 #'
 #' @return A ggplot theme object defining for use with [ggplot2::ggplot()].
 #' @export
-theme_polar <- function(guides = TRUE) {
+theme_polar <- function(guides = TRUE, panel_ontop = TRUE) {
   # make theme
   theme <-
     ggplot2::theme_minimal() %+replace%
     ggplot2::theme(
-      panel.ontop = T,
+      panel.ontop = panel_ontop,
       axis.line.x.top = ggplot2::element_blank(),
       panel.grid.minor.x = ggplot2::element_blank(),
       panel.grid.major.x = ggplot2::element_line(color = "grey75"),

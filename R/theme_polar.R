a6b052aff03c1a5c746fc39be0b303e4952e74df
Jack Davison <davison.jack.jd@gmail.com>
2022-11-15 13:01:07 +0000

    fix (theme_polar.R): remove guides argument
    Does more harm than good.

--- R/theme_polar.R
+++ R/theme_polar.R
@@ -2,18 +2,15 @@
 #'
 #' This is a complete theme which attempts to recreate the styling of
 #' [openair::polarPlot()]. As an alternative to using this theme, you could use
-#' [ggplot2::theme()] and [ggplot2::guides()].
+#' [ggplot2::theme()] or any of the in-built `ggplot2` themes.
 #'
-#' @param guides Resize the legend? Defaults to `TRUE`. `FALSE` may be useful if
-#'   you are controlling the legend yourself with [ggplot2::theme()] and/or
-#'   [ggplot2::guides()], but still want to apply the axis formatting.
 #' @param panel_ontop Place panel grids on top of the plot geometries? The
 #'   default, `TRUE`, can make polar plots easier to interpret, but this
 #'   behaviour may not always be desired.
 #'
 #' @return A ggplot theme object defining for use with [ggplot2::ggplot()].
 #' @export
-theme_polar <- function(guides = TRUE, panel_ontop = TRUE) {
+theme_polar <- function(panel_ontop = TRUE) {
   # make theme
   theme <-
     ggplot2::theme_minimal() %+replace%
@@ -25,20 +22,6 @@ theme_polar <- function(guides = TRUE, panel_ontop = TRUE) {
       panel.grid.major.y = ggplot2::element_line(linetype = 3, color = "grey75"),
       axis.text.x = ggplot2::element_text(face = "bold", size = 12)
     )
-
-  # optionally control guides
-  if (guides) {
-    theme <- list(
-      theme,
-      ggplot2::guides(
-        color = ggplot2::guide_colorbar(
-          frame.colour = "white",
-          ticks.colour = "white",
-          barheight = grid::unit(10, "cm")
-        )
-      )
-    )
-  }
 
   theme
 }

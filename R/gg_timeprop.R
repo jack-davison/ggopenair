f8b54a211f23685946399929a0881efd2f80e818
Jack Davison <davison.jack.jd@gmail.com>
2022-11-22 15:56:34 +0000

    feat (timeprop, trendlevel): add functions
    Added equivalents of timeProp and trendLevel.

---
+++ R/gg_timeprop.R
@@ -0,0 +1,113 @@
+#' Time series plot with categories shown as a stacked bar chart
+#'
+#' This function shows time series plots as stacked bar charts. The
+#' different categories in the bar chart are made up from a character
+#' or factor variable in a data frame. The function is primarily
+#' developed to support the plotting of cluster analysis output from
+#' \code{\link{polarCluster}} and \code{\link{trajCluster}} that
+#' consider local and regional (back trajectory) cluster analysis
+#' respectively. However, the function has more general use for
+#' understanding time series data.
+#'
+#' In order to plot time series in this way, some sort of time
+#' aggregation is needed, which is controlled by the option
+#' \code{avg_time}.
+#'
+#' The plot shows the value of \code{pollutant} on the y-axis
+#' (averaged according to \code{avg_time}). The time intervals are
+#' made up of bars split according to \code{proportion}. The bars
+#' therefore show how the total value of \code{pollutant} is made up
+#' for any time interval.
+#'
+#' @param data A data frame containing the fields \code{date},
+#'   \code{pollutant} and a splitting variable \code{proportion}
+#' @param pollutant Name of the pollutant to plot contained in
+#'   \code{mydata}.
+#' @param proportion The splitting variable that makes up the bars in
+#'   the bar chart e.g. \code{proportion = "cluster"} if the output
+#'   from \code{polarCluster} is being analysed. If \code{proportion}
+#'   is a numeric variable it is split into 4 quantiles (by default)
+#'   by \code{cutData}. If \code{proportion} is a factor or character
+#'   variable then the categories are used directly.
+#' @param avg_time This defines the time period to average to. Can be
+#'   \dQuote{sec}, \dQuote{min}, \dQuote{hour}, \dQuote{day},
+#'   \dQuote{DSTday}, \dQuote{week}, \dQuote{month}, \dQuote{quarter}
+#'   or \dQuote{year}. For much increased flexibility a number can
+#'   precede these options followed by a space. For example, a
+#'   timeAverage of 2 months would be \code{period = "2 month"}.
+#'
+#'   Note that \code{avg_time} when used in \code{timeProp} should be
+#'   greater than the time gap in the original data. For example,
+#'   \code{avg_time = "day"} for hourly data is OK, but
+#'   \code{avg_time = "hour"} for daily data is not.
+#' @param facet \code{facet} determines how the data are split i.e.
+#'   conditioned, and then plotted. The default is will produce a
+#'   single plot using the entire data. Type can be one of the
+#'   built-in types as detailed in \code{cutData} e.g. "season",
+#'   "year", "weekday" and so on. For example, \code{facet = "season"}
+#'   will produce four plots --- one for each season.
+#'
+#'   It is also possible to choose \code{facet} as another variable in
+#'   the data frame. If that variable is numeric, then the data will
+#'   be split into four quantiles (if possible) and labelled
+#'   accordingly. If facet is an existing character or factor
+#'   variable, then those categories/levels will be used directly.
+#'   This offers great flexibility for understanding the variation of
+#'   different variables and how they depend on one another.
+#'
+#'   \code{facet} must be of length one.
+#' @param normalise If \code{normalise = TRUE} then each time
+#'   interval is scaled to 100. This is helpful to show the relative
+#'   (percentage) contribution of the proportions.
+#' @export
+#' @author David Carslaw
+#' @seealso See \code{\link{timePlot}} for time series plotting,
+#'   \code{\link{polarCluster}} for cluster analysis of bivariate
+#'   polar plots and \code{\link{trajCluster}} for cluster analysis
+#'   of HYSPLIT back trajectories.
+
+gg_timeprop <- function(data, pollutant, proportion, avg_time = "day", facet = "default", normalise = FALSE) {
+  # run openair
+  oa_data <-
+    openair::timeProp(
+      mydata = data,
+      pollutant = pollutant,
+      proportion = "wd",
+      avg.time = avg_time,
+      type = facet,
+      plot = FALSE,
+      normalise = normalise
+    )$data
+
+  # create plot
+  plt <-
+    oa_data %>%
+    tidyr::drop_na(.data$var2) %>%
+    dplyr::mutate(
+      fill = forcats::fct_rev(.data[[proportion]]),
+      Var1 = dplyr::lag(.data$var2) %>% tidyr::replace_na(0)
+    ) %>%
+    ggplot2::ggplot(ggplot2::aes(
+      xmin = .data$xleft,
+      xmax = .data$xright,
+      ymin = .data$Var1,
+      ymax = .data$var2
+    )) +
+    ggplot2::geom_rect(ggplot2::aes(fill = .data$fill)) +
+    ggplot2::coord_cartesian(expand = FALSE) +
+    ggplot2::labs(
+      x = "date", y = openair::quickText(pollutant),
+      fill = openair::quickText(proportion)
+    )
+
+  # sort out facets
+  facet <- dplyr::group_vars(oa_data)
+  facet <- facet[!facet %in% c("xleft", "xright", "date")]
+  if (facet != "default") {
+    plt <-
+      plt + ggplot2::facet_wrap(facets = ggplot2::vars(.data[[facet]]))
+  }
+
+  # return plot
+  return(plt)
+}
---
+++ R/gg_trendlevel.R
@@ -0,0 +1,106 @@
+#' trendLevel
+#'
+#' The trendLevel function provides a way of rapidly showing a large amount of
+#' data in a condensed form. In one plot, the variation in the concentration of
+#' one pollutant can to shown as a function of three other categorical
+#' properties. The default version of the plot uses y = hour of day, x = month
+#' of year and type = year to provide information on trends, seasonal effects
+#' and diurnal variations. However, x, y and type and summarising statistics can
+#' all be modified to provide a range of other similar plots.
+#'
+#' \code{trendLevel} allows the use of third party summarising functions via the
+#' \code{statistic} option. Any additional function arguments not included
+#' within a function called using \code{statistic} should be supplied as a list
+#' of named parameters and sent using \code{stat.args}. For example, the encoded
+#' option \code{statistic = "mean"} is equivalent to \code{statistic = mean,
+#' stat.args = list(na.rm = TRUE)} or the R command \code{mean(x, na.rm= TRUE)}.
+#' Many R functions and user's own code could be applied in a similar fashion,
+#' subject to the following restrictions: the first argument sent to the
+#' function must be the data series to be analysed; the name `x' cannot be used
+#' for any of the extra options supplied in \code{stat.args}; and the function
+#' should return the required answer as a numeric or \code{NA}. Note: If the
+#' supplied function returns more than one answer, currently only the first of
+#' these is retained and used by \code{trendLevel}. All other returned
+#' information will be ignored without warning. If the function terminates with
+#' an error when it is sent an empty data series, the option
+#' \code{stat.safe.mode} should not be set to \code{FALSE} or \code{trendLevel}
+#' may fail. Note: The \code{stat.safe.mode = TRUE} option returns an NA without
+#' warning for empty data series.
+#'
+#' @param data The openair data frame to use to generate the \code{trendLevel}
+#'   plot.
+#' @param pollutant The name of the data series in \code{mydata} to sample to
+#'   produce the \code{trendLevel} plot.
+#' @param x,y,facet The name of the data series with which to bin \code{mydata}.
+#'   `x` and `y` form the x and y axes of the resulting plot, and `facet` is
+#'   optionally used to separate the plot into separate panels. Duplication in
+#'   \code{x}, \code{y} and \code{facet} are not permitted.
+#' @param statistic The statistic method to be use to summarise locally binned
+#'   \code{pollutant} measurements with. Three options are currently encoded:
+#'   \dQuote{mean} (default), \dQuote{max} and \dQuote{frequency}. (Note:
+#'   Functions can also be sent directly via \code{statistic}.  However, this
+#'   option is still in development and should be used with caution. See Details
+#'   below.)
+#' @param stat_args Additional options to be used with \code{statistic} if this
+#'   is a function. The extra options should be supplied as a list of named
+#'   parameters. (see Details below.)
+#' @param stat_safemode An addition protection applied when using functions
+#'   direclty with \code{statistic} that most users can ignore. This option
+#'   returns \code{NA} instead of running \code{statistic} on binned subsamples
+#'   that are empty. Many common functions terminate with an error message when
+#'   applied to an empty dataset. So, this option provides a mechanism to work
+#'   with such functions. For a very few cases, e.g. for a function that counted
+#'   missing entries, it might need to be set to \code{FALSE} (see Details
+#'   below.)
+#' @export
+#' @return As well as generating the plot itself, \code{trendLevel} also returns
+#'   an object of class ``openair''. The object includes three main components:
+#'   \code{call}, the command used to generate the plot; \code{data}, the data
+#'   frame of summarised information used to make the plot; and \code{plot}, the
+#'   plot itself. If retained, e.g. using \code{output <- trendLevel(mydata)},
+#'   this output can be used to recover the data, reproduce or rework the
+#'   original plot or undertake further analysis.
+#'
+#'   An openair output can be manipulated using a number of generic operations,
+#'   including \code{print}, \code{plot} and \code{summary}.
+#'
+#'   Summary statistics can also be extracted directly using \code{results},
+#'   e.g.  \code{results(object)} for \code{output <- trendLevel(mydata)}.
+
+gg_trendlevel <-
+  function(data,
+           pollutant,
+           x = "month",
+           y = "hour",
+           facet = "default",
+           statistic = c("mean", "max", "frequency"),
+           stat_args,
+           stat_safemode) {
+    oa_data <-
+      openair::trendLevel(
+        mydata = data,
+        pollutant = pollutant,
+        x = x,
+        y = y,
+        type = facet,
+        statistic = statistic,
+        stat.args = stat_args,
+        stat.safe.mode = stat_safemode,
+        plot = FALSE
+      )$data
+
+    oa_names <- names(oa_data)
+
+    plt <-
+      ggplot2::ggplot(oa_data, ggplot2::aes(x = .data[[oa_names[[1]]]], y = .data[[oa_names[[2]]]])) +
+      ggplot2::geom_tile(ggplot2::aes(fill = .data[[oa_names[[4]]]]))
+
+    if (oa_names[3] != "default") {
+      plt <- plt +
+        ggplot2::facet_wrap(ggplot2::vars(.data[[oa_names[[3]]]]))
+    }
+
+    return(plt)
+
+  }
+

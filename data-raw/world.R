fe69fd68c9f5c3c657cebe43b4f1944209c1caf9
Jack Davison <davison.jack.jd@gmail.com>
2023-04-12 18:35:54 +0100

    fix: remove world internal data
    no longer needed now maps are in openairmaps

--- data-raw/world.R
+++
@@ -1,5 +0,0 @@
-## code to prepare `world` dataset goes here
-
-world <- rnaturalearth::ne_countries(returnclass = "sf",scale = "medium")
-
-usethis::use_data(world, overwrite = TRUE, internal = TRUE)

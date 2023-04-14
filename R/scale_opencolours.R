e0bce3f9defb7384e349170660ea45c7727c0aed
Jack Davison <davison.jack.jd@gmail.com>
2023-04-14 08:21:56 +0100

    !feat: complete selection of colour scales
    adds discrete and binned openair colour scales
    
    breaks previous use of scale_opencolours_c as scale functions have now been standardised to scale_color_viridis

--- R/scale_opencolours.R
+++ R/scale_opencolours.R
@@ -1,37 +1,102 @@
-#' Plotting with openair palettes in ggplot2
+#' Various colour scales from `openair`
 #'
-#' This is a convenient wrapper around [ggplot2::scale_color_gradientn()] and
-#' [openair::openColours()] which creates colour gradients for `ggopenair`
-#' plots, with helpful defaults.
+#' The original `openair` package contained the [openair::openColours()]
+#' function which made many common colour palettes easily available. This is a
+#' `ggplot2` scale wrapper around this function similar to
+#' [ggplot2::scale_color_viridis_c()]. Unlike other `ggplot2` colour scale
+#' functions, the `aesthetics` argument defaults to both "fill" and "colour",
+#' meaning scales can be used with any `ggopenair` plotting function.
 #'
-#' @param colours The openair colour scheme to use. See [openair::openColours()]
-#'   for more information.
-#' @param na_value The aesthetic value to use for missing (NA) values. Defaults
-#'   to "grey50".
-#' @param aes Character string or vector of character strings listing the
-#'   name(s) of the aesthetic(s) that this scale works with. The default,
-#'   `c("colour", "fill")`, applies colours to both the `colour` *and* `fill`
-#'   aesthetics.
-#' @param ... Other arguments to pass to [ggplot2::scale_color_gradientn()].
-#'
-#' @return A ggproto object defining a continuous colour scale for use with
-#'   [ggplot2::ggplot()].
+#' @inheritParams openair::openColours
+#' @inheritParams ggplot2::scale_color_viridis_c
+#' @param ... Other arguments passed on to [ggplot2::discrete_scale()],
+#'   [ggplot2::continuous_scale()], or [ggplot2::binned_scale()] to control
+#'   name, limits, breaks, labels and so forth.
+#' @seealso The documentation on [colour
+#'   aesthetics][ggplot2::aes_colour_fill_alpha].
+#' @rdname scale_opencolours
 #' @export
-#'
-#' @examples
-#' \dontrun{
-#' polar_plot(mydata, "nox") + scale_opencolours()
-#' }
-#'
+scale_opencolours_d <-
+  function(...,
+           scheme = "default",
+           aesthetics = c("colour", "fill")) {
+    ggplot2::discrete_scale(aesthetics,
+                            "opencolours_d",
+                            openair_pal(colours = scheme),
+                            ...)
+  }
+
+#' @export
+#' @rdname scale_opencolours
 scale_opencolours_c <-
-  function(colours = "default",
-           na_value = "grey50",
-           aes = c("colour", "fill"),
-           ...) {
-    ggplot2::scale_color_gradientn(
-      colours = openair::openColours(scheme = colours, n = 100),
-      na.value = na_value,
-      aesthetics = aes,
+  function(...,
+           scheme = "default",
+           values = NULL,
+           space = "Lab",
+           na.value = "grey50",
+           guide = "colourbar",
+           aesthetics = c("colour", "fill")) {
+    ggplot2::continuous_scale(
+      aesthetics,
+      "opencolours_c",
+      scales::gradient_n_pal(openair_pal(colours = scheme)(10),
+                             values,
+                             space),
+      na.value = na.value,
+      guide = guide,
       ...
     )
   }
+
+#' @export
+#' @rdname scale_opencolours
+scale_opencolours_b <-
+  function(...,
+           scheme = "default",
+           values = NULL,
+           space = "Lab",
+           na.value = "grey50",
+           guide = "coloursteps",
+           aesthetics = c("colour", "fill")) {
+    pal <-
+      binned_pal(openair_pal(colours = scheme))
+
+    ggplot2::binned_scale(aesthetics,
+                          "opencolours_b",
+                          pal,
+                          na.value = na.value,
+                          guide = guide,
+                          ...)
+  }
+
+#' Similar to viridis_pal from scales
+#' @noRd
+openair_pal <-
+  function(colours = "default") {
+    function(n) {
+      openair::openColours(scheme = colours, n = n)
+    }
+  }
+
+#' @author ggplot2 authors
+#' @noRd
+binned_pal <- function (palette) {
+  function(x) {
+    palette(length(x))
+  }
+}
+
+#' @export
+#' @rdname scale_opencolours
+#' @usage NULL
+scale_opencolors_d <- scale_opencolours_d
+
+#' @export
+#' @rdname scale_opencolours
+#' @usage NULL
+scale_opencolors_c <- scale_opencolours_c
+
+#' @export
+#' @rdname scale_opencolours
+#' @usage NULL
+scale_opencolors_b <- scale_opencolours_b
